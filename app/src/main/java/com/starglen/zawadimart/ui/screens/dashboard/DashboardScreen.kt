fdc84434d7dc207547a8e817e55e67de08bad9f0
Aboudy-0 <aboudshiek17@gmail.com>
2025-04-09 12:57:06 +0300

    first commit

--- app/src/main/java/com/starglen/zawadimart/ui/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/starglen/zawadimart/ui/screens/dashboard/DashboardScreen.kt
@@ -1,6 +1,8 @@
 package com.starglen.zawadimart.ui.screens.dashboard
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -23,10 +27,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.starglen.zawadimart.R
 import com.starglen.zawadimart.ui.theme.neworange
 import com.starglen.zawadimart.ui.theme.newwhite
 
@@ -62,6 +69,7 @@ Column (modifier = Modifier.fillMaxSize().background(color = neworange)){
             .padding(start = 20.dp, end = 20.dp)
             .offset(y = 90.dp   )) {
 
+
         }
 
     }
@@ -72,9 +80,120 @@ Column (modifier = Modifier.fillMaxSize().background(color = neworange)){
     //Row
     Row (modifier = Modifier.padding(start = 20.dp)){
 
+        //Card1
+        Card(
+            modifier = Modifier.width(150.dp).height(180.dp)
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.home),
+                    contentDescription = "img",
+                    modifier = Modifier.size(50.dp)
+
+                )
+                Text(text = "Home", fontSize = 15.sp)
+
+
+            }
+
+
+        }
+        //End of Card1
+
+        Spacer(modifier = Modifier.width(50.dp))
+
+
+        //Card2
+        Card(
+            modifier = Modifier.width(150.dp).height(180.dp)
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.about),
+                    contentDescription = "img",
+                    modifier = Modifier.size(50.dp)
+
+                )
+                Text(text = "About", fontSize = 15.sp)
+
+
+            }
+
+
+        }
+        //End of Card2
+
     }
 
     //End of Row
+
+    Spacer(modifier = Modifier.height(30.dp))
+
+    //Row 2
+    Row (modifier = Modifier.padding(start = 20.dp)){
+
+        //Card1
+        Card(
+            modifier = Modifier.width(150.dp).height(180.dp)
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.contact),
+                    contentDescription = "img",
+                    modifier = Modifier.size(50.dp)
+
+                )
+                Text(text = "Contact", fontSize = 15.sp)
+
+
+            }
+
+
+        }
+        //End of Card1
+
+        Spacer(modifier = Modifier.width(50.dp))
+
+
+        //Card2
+        Card(
+            modifier = Modifier.width(150.dp).height(180.dp)
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.product),
+                    contentDescription = "img",
+                    modifier = Modifier.size(50.dp)
+
+                )
+                Text(text = "Product", fontSize = 15.sp)
+
+
+            }
+
+
+        }
+        //End of Card2
+
+    }
+
+    //End of Row 2
 
 
 
