20054d93a52abe0c18133bef0d52e6064dc1e513
Aboudy-0 <aboudshiek17@gmail.com>
2025-04-15 16:22:17 +0300

    commit

--- app/src/main/java/com/navigation/AppNavhost.kt
+++ app/src/main/java/com/navigation/AppNavhost.kt
@@ -2,10 +2,12 @@ package com.starglen.zawadimart.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.data.UserDatabase
 import com.navigation.ROUT_HOME
 import com.navigation.ROUT_ABOUT
 import com.navigation.ROUT_ASSIGNMENT
@@ -14,11 +16,16 @@ import com.navigation.ROUT_FORM
 import com.navigation.ROUT_FORM1
 import com.navigation.ROUT_INTENT
 import com.navigation.ROUT_ITEM
+import com.navigation.ROUT_LOGIN
+import com.navigation.ROUT_REGISTER
 import com.navigation.ROUT_SERVICE
 import com.navigation.ROUT_SPLASH
 import com.navigation.ROUT_START
+import com.repository.UserRepository
 import com.starglen.zawadimart.ui.screens.about.AboutScreen
 import com.starglen.zawadimart.ui.screens.assignment.AssignmentScreen
+import com.starglen.zawadimart.ui.screens.auth.LoginScreen
+import com.starglen.zawadimart.ui.screens.auth.RegisterScreen
 import com.starglen.zawadimart.ui.screens.dashboard.DashboardScreen
 import com.starglen.zawadimart.ui.screens.form1.Form1Screen
 import com.starglen.zawadimart.ui.screens.formscreen.FormScreen
@@ -29,6 +36,7 @@ import com.starglen.zawadimart.ui.screens.service.ServiceScreen
 import com.starglen.zawadimart.ui.screens.splash.SplashScreen
 import com.starglen.zawadimart.ui.screens.splash.SplashScreenPreview
 import com.starglen.zawadimart.ui.screens.start.StartScreen
+import com.viewmodel.AuthViewModel
 
 @Composable
 fun AppNavHost(
@@ -36,6 +44,8 @@ fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     startDestination: String = ROUT_FORM
 ) {
+
+    val context = LocalContext.current
 
     NavHost(
         navController = navController,
@@ -75,6 +85,28 @@ fun AppNavHost(
         composable(ROUT_FORM1) {
             Form1Screen(navController)
         }
+        //AUTHENTICATION
+
+        // Initialize Room Database and Repository for Authentication
+        val appDatabase = UserDatabase.getDatabase(context)
+        val authRepository = UserRepository(appDatabase.userDao())
+        val authViewModel: AuthViewModel = AuthViewModel(authRepository)
+        composable(ROUT_REGISTER) {
+            RegisterScreen(authViewModel, navController) {
+                navController.navigate(ROUT_LOGIN) {
+                    popUpTo(ROUT_REGISTER) { inclusive = true }
+                }
+            }
+        }
+
+        composable(ROUT_LOGIN) {
+            LoginScreen(authViewModel, navController) {
+                navController.navigate(ROUT_HOME) {
+                    popUpTo(ROUT_LOGIN) { inclusive = true }
+                }
+            }
+        }
+
 
     }
 }
