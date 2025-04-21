5a86c0e40776c7482f79bdfb3d8a52d798fc64bf
mkolksas <terermichael2@gmail.com>
2025-04-21 05:37:13 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.repository
+
+import com.data.UserDao
+import com.model.User
+
+class UserRepository(private val userDao: UserDao) {
+    suspend fun registerUser(user: User) {
+        userDao.registerUser(user)
+    }
+
+    suspend fun loginUser(email: String, password: String): User? {
+        return userDao.loginUser(email, password)
+    }
+}
